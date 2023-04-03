0b29cc54a815b23fcb27c68503bade6f38816c31
alelorena <alexandre.lorena@gmail.com>
2023-04-03 17:37:32 -0300

    teste de commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
-public class App {
-    public static void main(String[] args) throws Exception {
+public class TiposVariaveis {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Teste");
     }
+  
 }
+/* */
