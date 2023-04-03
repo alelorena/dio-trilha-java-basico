d03a72ac67ef7103ae23ea2290a628f3185d4f9b
alelorena <alexandre.lorena@gmail.com>
2023-04-03 18:37:31 -0300

    atualizando exemplo de variaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores {
+                //classe Operadores.java
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+
+    }
+
+
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,6 +2,21 @@ public class TiposVariaveis {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Teste");
+            double salarioMinimo = 2500;
+
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short)numeroNormal;
+
+            int numero = 1;
+            numero = 2;
+            System.out.println(numero);
+
+            final double VALOR_DE_PI = 3.14;
+            System.out.println(VALOR_DE_PI);
+
+            
+
     }
   
 }
