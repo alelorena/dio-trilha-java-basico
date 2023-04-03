e32c1b86f5dba223fc02e7ecd244bb9db0ef1286
alelorena <alexandre.lorena@gmail.com>
2023-04-03 18:44:21 -0300

    corrreção em operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,8 @@
 public class Operadores {
                 //classe Operadores.java
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-		
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+		System.out.println(nomeCompleto);
         //qual o resultado das expressoes abaixo?
         String concatenacao ="?"; 
 
